be45a03ac81ec52fc6ba219262f1125e3c4b5425
sooriya-25 <sooriyaramakrishnan08@gmail.com>
2026-08-06 09:32:55 +0530

    Day 27: dynamic import using lazy loading

--- Day-27/frontend/src/routes/AppRoutes.jsx
+++ Day-27/frontend/src/routes/AppRoutes.jsx
@@ -1,21 +1,37 @@
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { Spin } from "antd";
+
 import ProtectedRoute from "./ProtectedRoute";
-
-import Login from "../pages/Login/Login";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import Stocks from "../pages/Stocks/Stocks";
-import Watchlist from "../pages/Watchlist/Watchlist";
-import Users from "../pages/Users/Users";
-import Profile from "../pages/Profile/Profile";
-
 import MainLayout from "../layouts/MainLayout";
+
+const Login = lazy(() => import("../pages/Login/Login"));
+const Dashboard = lazy(() => import("../pages/Dashboard/Dashboard"));
+const Stocks = lazy(() => import("../pages/Stocks/Stocks"));
+const Watchlist = lazy(() => import("../pages/Watchlist/Watchlist"));
+const Users = lazy(() => import("../pages/Users/Users"));
+const Profile = lazy(() => import("../pages/Profile/Profile"));
+
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={
+            <Suspense
+              fallback={
+                <div className="flex justify-center py-4">
+                  <Spin />
+                </div>
+              }
+            >
+              <Login />
+            </Suspense>
+          }
+        />
 
         <Route
           path="/"
@@ -25,22 +41,82 @@ const AppRoutes = () => {
             </ProtectedRoute>
           }
         >
-          <Route index element={<Dashboard />} />
+          <Route
+            index
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex justify-center py-4">
+                    <Spin />
+                  </div>
+                }
+              >
+                <Dashboard />
+              </Suspense>
+            }
+          />
 
-          <Route path="stocks" element={<Stocks />} />
+          <Route
+            path="stocks"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex justify-center py-4">
+                    <Spin />
+                  </div>
+                }
+              >
+                <Stocks />
+              </Suspense>
+            }
+          />
 
-          <Route path="watchlist" element={<Watchlist />} />
+          <Route
+            path="watchlist"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex justify-center py-4">
+                    <Spin />
+                  </div>
+                }
+              >
+                <Watchlist />
+              </Suspense>
+            }
+          />
 
           <Route
             path="users"
             element={
               <ProtectedRoute roles={["ADMIN"]}>
-                <Users />
+                <Suspense
+                  fallback={
+                    <div className="flex justify-center py-4">
+                      <Spin />
+                    </div>
+                  }
+                >
+                  <Users />
+                </Suspense>
               </ProtectedRoute>
             }
           />
 
-          <Route path="profile" element={<Profile />} />
+          <Route
+            path="profile"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex justify-center py-4">
+                    <Spin />
+                  </div>
+                }
+              >
+                <Profile />
+              </Suspense>
+            }
+          />
         </Route>
 
         <Route path="*" element={<Navigate to="/" replace />} />
